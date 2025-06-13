c7175325e9350f9a4c6c2b52894ecf7139f44b08
mattbedford <matt.bedford@gmail.com>
2025-06-13 23:54:20 +0200

    update language switch logic

--- assets/js/language-switcher.js
+++ assets/js/language-switcher.js
@@ -1,13 +1,24 @@
 const browserLang = navigator.language || navigator.userLanguage;
 console.log('Detected browser language:', browserLang);
 
+function handleLanguageRedirect(language) {
+    if (language.startsWith('en')) {
+        window.location.href = '/en/';
+    } else if (language.startsWith('it')) {
+        window.location.href = '/';
+    } else {
+        window.location.href = '/en/';
+    }
+}
+
 const languageSelectors = document.querySelectorAll('#languageSelectNav, #languageSelectFooter');
 languageSelectors.forEach(selector => {
     selector.addEventListener('change', (e) => {
         const selectedLang = e.target.value;
         localStorage.setItem('preferredLanguage', selectedLang);
         console.log('Language switched to:', selectedLang);
-        // Future: trigger language switch functionality
+        handleLanguageRedirect(selectedLang);
+
         languageSelectors.forEach(el => el.value = selectedLang);
     });
 });
