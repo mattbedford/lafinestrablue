ffdac6077001d528bfa14c30e14df24305d10844
mattbedford <matt.bedford#gmail.com>
2025-06-14 12:49:44 +0200

    Add styles and swiper thumbs

--- assets/js/swiper-init.js
+++ assets/js/swiper-init.js
@@ -1,28 +1,41 @@
 <!-- Swiper Initialization -->
 document.addEventListener("DOMContentLoaded", function () {
     const swipers = [
-        {selector: "#swiper-rose"},
-        {selector: "#swiper-lavender"},
-        {selector: "#swiper-sunlight"},
-        {selector: "#swiper-gallery"}
+        {selector: "#swiper-stroke"},
+        {selector: "#swiper-hug"},
+        {selector: "#swiper-smile"},
+        {selector: "#swiper-gallery"},
+        {selector: "#swiper-stroke-thumbs"},
+        {selector: "#swiper-hug-thumbs"},
+        {selector: "#swiper-smile-thumbs"},
+        {selector: "#swiper-gallery-thumbs"}
     ];
 
     swipers.forEach(({selector}) => {
         const swiperElement = document.querySelector(selector);
         if (swiperElement) {
-            new Swiper(swiperElement, {
-                loop: true,
-                spaceBetween: 10,
-                slidesPerView: 1,
-                navigation: {
-                    nextEl: `${selector} .swiper-button-next`,
-                    prevEl: `${selector} .swiper-button-prev`
-                },
-                pagination: {
-                    el: `${selector} .swiper-pagination`,
-                    clickable: true
-                }
-            });
+            if (selector.includes('thumbs')) {
+                new Swiper(swiperElement, {
+                    loop: true,
+                    spaceBetween: 10,
+                    slidesPerView: 4,
+                    freeMode: true,
+                    watchSlidesProgress: true,
+                });
+            } else {
+                new Swiper(swiperElement, {
+                    loop: true,
+                    spaceBetween: 10,
+                    slidesPerView: 1,
+                    navigation: {
+                        nextEl: `${selector} .swiper-button-next`,
+                        prevEl: `${selector} .swiper-button-prev`
+                    },
+                    thumbs: {
+                        swiper: `${selector}-thumbs`,
+                    },
+                });
+            }
         }
     });
 });
